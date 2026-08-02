42be74b915eeefbc61648eb4972f01a21a8ccaff
D.M.D.T.R.Dissanayaka <Damith-itt033@users.noreply.github.com>
2026-08-02 09:45:10 +0530

    create common helpers file

---
+++ includes/functions.php
@@ -0,0 +1,14 @@
+<?php
+// includes/functions.php
+
+/**
+ * Clean and sanitize user input for security.
+ */
+function sanitize_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data, ENT_QUOTES, 'UTF-8');
+    return $data;
+}
+
+/**
